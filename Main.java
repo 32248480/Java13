644ecb3128837c20112da2c0b16c6f7389f5697d
32248480 <92283556+32248480@users.noreply.github.com>
2021-12-24 01:52:07 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+public static void main(String args[]){
+try{
+ int num=12,den=0;
+ int ans=num/den;
+ System.out.println("ans="+ans);
+}
+catch(ArithmeticException e){
+ System.out.println("除數為 0");
+}
+finally{
+ System.out.println("end of main() method!!");
+}
+}
+}
+
+//除數為 0
+//end of main() method!!	
